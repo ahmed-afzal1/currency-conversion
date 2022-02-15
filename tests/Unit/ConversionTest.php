14261e0ad7e2be742af9e3488855f9875ce91127
ahmed-afzal1 <ahmmedafzal4@gmail.com>
2022-02-15 08:22:27 +0600

    Conversion Testing

--- tests/Unit/ConversionTest.php
+++ tests/Unit/ConversionTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Unit;
 
+use App\Repository\CurrencyRepositoryInterface;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
 
 class ConversionTest extends TestCase
 {
+
     /**
      * A basic unit test example.
      *
@@ -13,6 +16,14 @@ class ConversionTest extends TestCase
      */
     public function test_example()
     {
-        $this->assertTrue(true);
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get('http://apilayer.net/api/live?access_key=efe459e8db765f08b29c65ea562c4f9a&source=USD');
+        $response = $request->getBody()->getContents();
+
+        $str=str_replace("\r\n","",$response);
+        $currencies = json_decode($str, true);
+
+
+        $this->assertContains($currencies['quotes']);
     }
 }
